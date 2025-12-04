b1ab7ae93c3de28b4f568ff5a0d7a889b42e53cc
Jeevan M Swamy <jeevanms003@gmail.com>
2025-12-04 19:29:54 +0530

    fixed

--- 1.tcl
+++ 1.tcl
@@ -1,58 +1,46 @@
-# Create simulator
-set ns [new Simulator]
+set ns [ new Simulator ]
 
-# Create trace files
-set nam [open out.nam w]
+set nam [ open out.nam w]
 $ns namtrace-all $nam
 
-set tr [open out.tr w]
-$ns trace-all $tr
+set tr [ open out.tr w ]
+$ns trace-all $tr 
 
-# Create 3 nodes
-set n0 [$ns node]
-set n1 [$ns node]
-set n2 [$ns node]
+set n0 [ $ns node ]
+set n1 [ $ns node ]
+set n2 [ $ns node ]
 
-# Duplex links (bandwidth can be varied)
 $ns duplex-link $n0 $n1 20Mb 10ms DropTail
 $ns duplex-link $n1 $n2 5Mb 10ms DropTail
 
-# Queue sizes
 $ns queue-limit $n0 $n1 3
 $ns queue-limit $n1 $n2 3
 
-# Create TCP agent and sink
-set tcp [new Agent/TCP]
-set sink [new Agent/TCPSink]
+set tcp [ new Agent/TCP ]
+set sink [ new Agent/TCPSink ]
 
 $ns attach-agent $n0 $tcp
 $ns attach-agent $n2 $sink
 $ns connect $tcp $sink
 
-# CBR application over TCP 
-set cbr [new Application/Traffic/CBR]
-$cbr set packetSize_ 1000
-$cbr set interval_ 0.005
+set cbr [ new Application/Traffic/CBR ]
 $cbr attach-agent $tcp
 
-# Finish procedure
 proc finish {} {
-    global ns nam tr
-    $ns flush-trace
-    close $tr
-    close $nam
 
-    exec nam out.nam &
-    exec echo "The number of packets dropped: " &
-    exec grep -c "^d" out.tr &
-    exit 0
+	global ns nam tr 
+	$ns flush-trace
+	close $tr
+	close $nam
+	
+	exec nam out.nam &
+	exec echo "The number of packets dropped are: " &
+	exec grep -c "^d" out.tr &
+	exit 0
 }
 
-# Start and stop traffic
 $ns at 0.1 "$cbr start"
 $ns at 1.0 "$cbr stop"
-
-# End simulation
 $ns at 1.5 "finish"
 $ns run
 
