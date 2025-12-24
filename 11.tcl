d09af7e28e59e583e49b108c1d2d1c102f124607
Jeevan M Swamy <jeevanms003@gmail.com>
2025-12-24 16:24:50 +0530

    Update 11.tcl

--- 11.tcl
+++ 11.tcl
@@ -90,7 +90,7 @@ proc finish {} {
     $ns flush-trace
     close $nt
     exec awk -f Lab5.awk Lab5.tr &
-    exec xgraph -P -x TIME -y DATA -tkFont fixed gsm.xg &
+    exec xgraph -P -x TIME -y DATA -tf fixed -lf fixed gsm.xg &
     exit 0
 }
 
